8d25af2b4c9676d7bab2cb33d0d3559d4038fc63
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2010-11-30 00:34:49 +0100

    counting really steps fixed

--- lib/fuubar-cucumber.rb
+++ lib/fuubar-cucumber.rb
@@ -7,6 +7,7 @@ module Cucumber
       def initialize(step_mother, path_or_io, options)
         @step_count = @finished_count = @issues_count = 0
         @in_background = false
+        @is_example = false
         super
       end
 
@@ -15,11 +16,11 @@ module Cucumber
         @progress_bar = ProgressBar.new("  #{@step_count} steps", @step_count, @io)
         @progress_bar.bar_mark = '='
       end
-      
+
       def before_background(background)
         @in_background = true
       end
-      
+
       def after_background(background)
         @in_background = false
       end
@@ -32,7 +33,7 @@ module Cucumber
       end
 
       def after_step_result(keyword, step_match, multiline_arg, status, exception, source_indent, background)
-        return if @in_background
+        return if @in_background || status == :skipped
         @state = :red if status == :failed
         if [:failed, :undefined].include? status
           @io.print "\e[K"
@@ -44,6 +45,33 @@ module Cucumber
           @io.flush
         end
         super
+      end
+      
+      def before_examples(examples)
+        @is_example = true
+      end
+      
+      def after_exaples(examples)
+        @is_example = false
+      end
+      
+      def before_table_row(table_row)
+        if @is_example && table_row.scenario_outline
+          progress('passed', table_row.scenario_outline.instance_variable_get("@background").instance_variable_get("@steps").count)
+        end
+      end
+      
+      def after_table_row(table_row)
+        if @is_example && table_row.scenario_outline
+          progress('passed', table_row.scenario_outline.instance_variable_get("@steps").count)
+        end
+      end
+      
+      def table_cell_value(value, status)
+        # return unless @outline_table
+        # p "c #{value} - #{status}"
+        # status ||= @status
+        # progress(status) unless table_header_cell?(status)
       end
 
       protected
@@ -68,10 +96,10 @@ module Cucumber
 
         COLORS = { :green =>  "\e[32m", :yellow => "\e[33m", :red => "\e[31m" }
 
-        def progress(status)
+        def progress(status = 'passed', count = 1)
           @io.print COLORS[state]
-          @finished_count += 1
-          @progress_bar.inc
+          @finished_count += count
+          @progress_bar.inc(count)
           @progress_bar.instance_variable_set("@title", "  #{@finished_count}/#{@step_count}")
           @io.print "\e[0m"
         end
@@ -80,19 +108,26 @@ module Cucumber
           count = 0
           features = features.instance_variable_get("@features")
           features.each do |feature|
-            #get scenarios
+            if feature.instance_variable_get("@background")
+              background = feature.instance_variable_get("@background")
+              background.init
+              background_steps = background.instance_variable_get("@steps").instance_variable_get("@steps")
+            end
             feature.instance_variable_get("@feature_elements").each do |scenario|
               scenario.init
-              #get steps
               steps = scenario.instance_variable_get("@steps").instance_variable_get("@steps")
-              count += steps.size
-              #get example table
+              scenario_size = steps.size
+              if background_steps && scenario.is_a?(Cucumber::Ast::ScenarioOutline)
+                background_size = background_steps.size
+              end
               examples = scenario.instance_variable_get("@examples_array")
-              unless examples.nil?
+              if examples
                 examples.each do |example|
                   example_matrix = example.instance_variable_get("@outline_table").instance_variable_get("@cell_matrix")
-                  count += example_matrix.size
+                  count += (scenario_size + background_size)*(example_matrix.size - 1)
                 end
+              else
+                count += scenario_size
               end
             end
           end
