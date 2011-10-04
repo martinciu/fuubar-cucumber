3df5df5bd73beace6d237af0aa40f61588e56e1a
dbloete <mail@dennisreimann.de>
2011-10-04 11:36:18 +0200

    Alias the formatter as fuubar. Closes #4.

--- lib/fuubar-cucumber.rb
+++ lib/fuubar-cucumber.rb
@@ -1,1 +1,8 @@
 require 'cucumber/formatter/fuubar'
+
+# Extend Cucumber's builtin formats, so that this
+# formatter can be used with --format fuubar
+Cucumber::Cli::Options::BUILTIN_FORMATS["fuubar"] = [
+  "Cucumber::Formatter::Fuubar",
+  "The instafailing Cucumber progress bar formatter"
+]
