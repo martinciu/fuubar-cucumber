4af981f038332a27aba76fd3f81920688f0dd196
Dennis Reimann <mail@dennisbloete.de>
2011-10-04 13:21:26 +0300

    CLI Options need to be required before modification

--- lib/fuubar-cucumber.rb
+++ lib/fuubar-cucumber.rb
@@ -2,6 +2,8 @@ require 'cucumber/formatter/fuubar'
 
 # Extend Cucumber's builtin formats, so that this
 # formatter can be used with --format fuubar
+require 'cucumber/cli/options'
+
 Cucumber::Cli::Options::BUILTIN_FORMATS["fuubar"] = [
   "Cucumber::Formatter::Fuubar",
   "The instafailing Cucumber progress bar formatter"
